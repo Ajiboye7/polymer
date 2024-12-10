830c57d991ea0c1d9d4627cef6fb3603a6ec8d69
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-12-10 18:40:21 +0100

    icons added

--- constant/index.ts
+++ constant/index.ts
@@ -2,6 +2,32 @@ import home from "@/assets/icons/home.png";
 import card from "@/assets/icons/card.png";
 import payment from "@/assets/icons/send-2.png";
 import profile from "@/assets/icons/user-octagon.png";
+import accessBank from "@/assets/icons/Access Bank svg.png";
+import check from "@/assets/icons/fa_check.png";
+import cancel from "@/assets/icons/foundation_x.png";
+import arrowLeft from "@/assets/icons/Arrow Left.png";
+import user from "@/assets/icons/user-octagon.png";
+import add from "@/assets/icons/add.png";
+import live from "@/assets/icons/Frame (1).png";
+import gtBank from "@/assets/icons/GUARANTY Trust Bank - jpeg.png";
+import setting from "@/assets/icons/information.png";
+import opay from "@/assets/icons/OPay - png.png";
+import facebook from "@/assets/icons/path16.png";
+import tick from "@/assets/icons/shield-tick.png";
+import send from "@/assets/icons/send-2.png";
+import rotateLeft from "@/assets/icons/rotate-left.png";
+import eye from "@/assets/icons/Group.png";
+import nigeria from "@/assets/icons/Vector (1).png";
+import instagram from "@/assets/icons/Instagram.png";
+import quote from "@/assets/icons/glass.png";
+import icon from "@/assets/icons/Frame (1).png";
+
+import barCode from "@/assets/images/bar-code-svgrepo-com.png"
+import faceScan from "@/assets/images/face scan.png"
+import oops from "@/assets/images/Frame 14135.png"
+import warning from "@/assets/images/Group 2098.png"
+import splash from "@/assets/images/splash.png"
+
 
 
 
@@ -10,4 +36,32 @@ export const icons= {
     card,
     payment,
     profile,
+    accessBank,
+    check,
+    cancel,
+    arrowLeft,
+    user,
+    add,
+    live,
+    gtBank,
+    setting,
+    opay,
+    facebook,
+    tick,
+    send,
+    rotateLeft,
+    eye,
+    nigeria,
+    instagram,
+    quote,
+    icon
+
+}
+
+export const images ={
+    barCode,
+    faceScan,
+    oops,
+    warning,
+    splash,
 }
